c6f0edb5276850fc344a43b7df6d08a6624e1d31
Varun-San <varun.v@saneforce.io>
2025-03-26 18:00:19 +0530

    added the layout

---
+++ src/app/Components/Layout/layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NgModel } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-layout',
+  imports: [RouterModule],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.css',
+})
+export class LayoutComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,51 +7,58 @@ import { AlldoneComponent } from './Components/Login Pages/alldone/alldone.compo
 import { ConfigurationComponent } from './Components/Setup/Configuration/configuration.component';
 import { UserWiseSettingsComponent } from './Components/Setup/Configuration/User Wise Settings/user-wise-settings.component';
 import { GeneralSettingsComponent } from './Components/Setup/Configuration/General Settings/general-settings.component';
+import { LayoutComponent } from './Components/Layout/layout.component';
 
 export const routes: Routes = [
   {
-    //Default route
+    // Default route
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
   },
   {
-    //Home-Page
+    // Home-Page
     path: 'home',
     component: SampleComponent,
   },
   {
-    //Forgot-Password
+    // Forgot-Password
     path: 'forgotpassword',
     component: ForgotPasswordComponent,
   },
   {
-    //Verification
+    // Verification
     path: 'verification',
     component: VerificationComponent,
   },
   {
-    //New-Password
+    // New-Password
     path: 'newpassword',
     component: NewPasswordComponent,
   },
   {
-    //All-Done
+    // All-Done
     path: 'password_alldone',
     component: AlldoneComponent,
   },
-  // Setup --> Configuration --> General Settings
   {
-    path: 'configuration',
-    component: ConfigurationComponent,
+    // Routes for the layout section (without "layout" prefix)
+    path: '',
+    component: LayoutComponent,
     children: [
       {
-        path: 'general-settings',
-        component: GeneralSettingsComponent,
-      },
-      {
-        path: 'user-settings',
-        component: UserWiseSettingsComponent,
+        path: 'configuration',
+        component: ConfigurationComponent,
+        children: [
+          {
+            path: 'general-settings',
+            component: GeneralSettingsComponent,
+          },
+          {
+            path: 'user-settings',
+            component: UserWiseSettingsComponent,
+          },
+        ],
       },
     ],
   },
